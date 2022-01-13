898841c51366d32d7f238e491dd0b5d154db87f4
Pranali <pranalilambat00@gmail.com>
2022-01-13 08:40:15 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LineComparisonProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+            //UC1 uc1 = new UC1();
+            //UC1.CartesianProgram();
+
+            //UC2 uc2 = new UC2();
+            //UC2.EqualsProgram();
+
+            //UC3 uc3 = new UC3();
+            //UC3.ComparisionOfLine();
+
+        }
+    }
+}
